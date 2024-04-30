dc6e2ccfab6a7a98e6c89b933a7dd9c1c43a94ca
UltimaVanguard <ultimavanguard@gmail.com>
2024-04-30 11:05:15 -0500

    added id to show note when clicked and added delete functionality

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const notes = require('express').Router();
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 
 notes.get('/', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
@@ -12,6 +13,7 @@ notes.post('/', (req, res) => {
         const newNote = {
             text,
             title,
+            id: uuidv4()
         }
 
         readAndAppend(newNote, './db/db.json');
@@ -25,6 +27,16 @@ notes.post('/', (req, res) => {
     } else {
         res.json('Error in posting note');
     }
+})
+
+notes.delete('/:id', (req, res) => {
+        readFromFile('./db/db.json')
+            .then((data) => {
+                let newData = JSON.parse(data);
+                let notes = newData.filter((note) => note.id !== req.params.id)
+                writeToFile('./db/db.json', notes);
+                res.json(notes);
+        })
 })
 
 module.exports = notes;
